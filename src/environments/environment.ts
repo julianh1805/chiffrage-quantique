f46899ae8d6ca68f659ecfee1c0b349c8cb5d33b
ncev <nicolaseven76@gmail.com>
2021-11-24 21:56:51 +0100

    (chore) adding environment host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// require('dotenv').config();
+
 export const environment = {
-  production: false
+  production: false,
+  host: 'http://localhost:5000'
 };
 
 /*
